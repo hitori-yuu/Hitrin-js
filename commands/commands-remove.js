05f4ee70f0d7fe0ef36cffaff25a58aa6bce0c75
hitori-yuu <yuu.hitorin@gmail.com>
2021-11-27 17:01:46 +0900

    [EDIT] ready.js [ADD] commands-remove.js

---
+++ commands/commands-remove.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('del')
+		.setDescription('指定のコマンドをすべて削除。')
+		.addStringOption(option => option.setName('種類').setDescription('種類を選択').addChoice('ギルドコマンド', 'guild').addChoice('アプリケーションコマンド', 'application')),
+	async execute(interaction, client) {
+		if (!interaction.user.id === '874184214130602015') return;
+		const type = interaction.options.getString('種類');
+		if (type === 'guild') {
+			interaction.guild.commands.fetch()
+				.then(commands => {
+					for (let i = 0; i < commands.size; i++) {
+						commands.delete();
+					}
+					interaction.reply('このサーバのギルドコマンドをすべて削除しました。');
+				})
+				.catch(console.error);
+		}
+		if (type === 'application') {
+			client.application.commands.fetch()
+				.then(commands => {
+					for (let i = 0; i < commands.size; i++) {
+						commands.delete();
+					}
+					interaction.reply('アプリケーションコマンドをすべて削除しました。');
+				})
+				.catch(console.error);
+		}
+	},
+};
--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,10 @@ module.exports = {
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		setInterval(() => {
-			client.user.setActivity(`/help | 1.0.1v | ${client.guilds.cache.size} servers | ${client.users.cache.size} users`, { type: 'PLAYING' }, { status: 'online' });
+			client.user.setActivity(`/help | 1.0.2v | ${client.guilds.cache.size} サーバー | ${client.users.cache.size} ユーザー`, { type: 'PLAYING' }, { status: 'online' });
 		}, 10000);
 	},
 };
 
 // client.user.setActivity(`/help | ${client.guilds.cache.size} servers | ${client.users.cache.size} users`, { type: 'PLAYING' }, { status: 'online' });
+// client.user.setActivity(`/help | 1.0.1v | ${client.guilds.cache.size} servers | ${client.users.cache.size} users`, { type: 'PLAYING' }, { status: 'idle' });
