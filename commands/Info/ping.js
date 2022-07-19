dbbda6681a4a53f9ee160748c2a09622a183e8a8
hitori-yuu <yuu.hitorin@gmail.com>
2022-07-19 23:06:37 +0900

    [ADD] Main body

---
+++ commands/Info/ping.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    description: "Returns websocket latency",
+
+    run: async (client, interaction) => {
+        const embed = new EmbedBuilder()
+            .setColor("#FF0000")
+            .setTitle("🏓 Pong!")
+            .setDescription(`Latency : ${client.ws.ping}ms`)
+            .setTimestamp()
+            .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
+        interaction.followUp({ embeds: [embed] });
+    },
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const { Client, Partials, Collection, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [
+        Partials.User,
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.GuildScheduledEvent,
+        Partials.ThreadMember,
+    ]
+});
+module.exports = client;
+
+// Global Variables
+client.commands = new Collection();
+client.slashCommands = new Collection();
+
+// Initializing the project
+require("./handler")(client);
+
+client.login(process.env.TOKEN);
