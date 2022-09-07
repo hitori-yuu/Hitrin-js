eebd629c4ad85a6acb93280d63d501ab5ce13861
ヒトリユウ <yuu.hitorin@gmail.com>
2022-09-07 16:46:01 +0900

    [EDIT] messageDelete.js [ADD] messageUpdate.js

--- logging/messageUpdate.js
+++ logging/messageUpdate.js
@@ -2,21 +2,20 @@ const { EmbedBuilder } = require('discord.js');
 const logsChannelsModel = require('../models/logsChannelsSchema');
 
 module.exports = {
-	name: 'messageDelete',
+	name: 'messageUpdate',
 
-	async execute(message) {
-        const AuditLogs = await message.guild.fetchAuditLogs({ limit: 1 });
+	async execute(oldMessage, newMessage) {
+        const AuditLogs = await oldMessage.guild.fetchAuditLogs({ limit: 1 });
 
         const log = AuditLogs.entries.first();
-        const member = message.guild.members.cache.get(log.executor.id);
-        const target = message.guild.members.cache.get(log.target.id);
+        const member = oldMessage.guild.members.cache.get(log.executor.id);
 
         const logEmbed = new EmbedBuilder()
             .setColor('#59b9c6')
             .setAuthor({ name: member.user.tag, iconURL: member.displayAvatarURL({extension: 'png'}) })
-            .setTitle('メッセージ削除')
+            .setTitle('メッセージ更新')
             .setDescription(
-                `<@${member.id}> が <@${target.id}> のメッセージを削除しました。`
+                `<@${member.id}> がメッセージを更新しました。`
             )
             .addFields(
                 {
@@ -24,16 +23,16 @@ module.exports = {
                     value: `**[名前]** ${log.extra.channel.name}\n**[ID]** ${log.extra.channel.id}`
                 },
                 {
-                    name: '__**メッセージ送信者:**__',
-                    value: `**[名前]** ${target.user.tag}\n**[ID]** ${target.id}`
+                    name: '__**メッセージ:**__',
+                    value: `**[更新前]**\n${oldMessage.content}\n**[更新後]**\n${newMessage.content}`
                 },
             )
             .setTimestamp()
             .setFooter({ text: '© 2021-2022 HitoriYuu, Hitrin' });
 
-        const guildsData = await logsChannelsModel.findOne({ id: message.guild.id });
+        const guildsData = await logsChannelsModel.findOne({ id: oldMessage.guild.id });
         if (guildsData) {
-            message.guild.channels.cache.get(guildsData.channel.id).send({embeds: [logEmbed]});
+            oldMessage.guild.channels.cache.get(guildsData.channel.id).send({embeds: [logEmbed]});
         } else {
             return;
         }
