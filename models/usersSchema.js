ee4ed4a368dd553ae1a1f3f3152239af2d38063f
ヒトリユウ <yuu.hitorin@gmail.com>
2022-09-08 19:46:07 +0900

    [EDIT] [ADD] [EDIT] interactionCreate.js [ADD] usersSchema.js initialSetting.js

---
+++ models/usersSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+    id: { type: String },
+    name: { type: String },
+    tos: { type: Boolean },
+    evaluation: { type: Number, min: 0, max: 10 },
+    profile: {
+        avatar: { type: String },
+        color: { type: String },
+        description: { type: String },
+        birthday: {
+            date: { type: Date },
+            public: { type: Boolean },
+        }
+    },
+    createDate : { type: String },
+});
+
+const model = mongoose.model('Users', usersSchema);
+
+module.exports = model;
