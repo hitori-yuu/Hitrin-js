87ced716342d5223553a0a4c5f743dd9d1f7afa1
ヒトリユウ <yuu.hitorin@gmail.com>
2022-09-19 22:06:50 +0900

    [EDIT] events

--- bot/logging/emojiUpdate.js
+++ bot/logging/emojiUpdate.js
@@ -34,7 +34,7 @@ module.exports = {
 
             const guildsData = await logsChannelsModel.find();
             const data = guildsData.filter(data => data.guild.id === oldEmoji.guild.id);
-            if (!data) {
+            if (!data || !data.length > 0) {
                 return;
             } else {
                 oldEmoji.guild.channels.cache.get(data[0].channel.id).send({embeds: [logEmbed]});
