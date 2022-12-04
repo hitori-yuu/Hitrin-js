2a0dda54ce7a3e8fe8a25670dd12eeed2d4e7939
ヒトリユウ <yuu.hitorin@gmail.com>
2022-12-05 00:16:18 +0900

    [EDIT] linkContent.js guildsSchema.js

--- bot/events/linkContent.js
+++ bot/events/linkContent.js
@@ -1,4 +1,4 @@
-const { ChannelType, AttachmentBuilder } = require('discord.js');
+const { ChannelType, AttachmentBuilder, EmbedBuilder } = require('discord.js');
 const { Error } = require('../handlers/error');
 const Canvas = require('canvas');
 const path = require('path');
@@ -17,43 +17,54 @@ module.exports = {
                 const Channel = Guild.channels.cache.get(messages[5]);
                 await Channel.messages.fetch(messages[6]).then(async msg => {
                     if (!msg.content) return;
-                    const canvas = Canvas.createCanvas(800, 100);
-                    const ctx = canvas.getContext('2d');
-                    const background = await Canvas.loadImage(
-                        path.join(__dirname, '../assets/background.jpg')
-                    )
-                    let x = 0;
-                    let y = 0;
-                    ctx.drawImage(background, x, y);
+                    // const canvas = Canvas.createCanvas(800, 100);
+                    // const ctx = canvas.getContext('2d');
+                    // const background = await Canvas.loadImage(
+                    //     path.join(__dirname, '../assets/background.jpg')
+                    // )
+                    // let x = 0;
+                    // let y = 0;
+                    // ctx.drawImage(background, x, y);
 
-                    const pfp = await Canvas.loadImage(
-                        msg.author.displayAvatarURL({extension: 'png'})
-                    );
-                    x = canvas.width / 5.8 - pfp.width;
-                    y = canvas.height / 2 - pfp.height / 2;
-                    ctx.arc(50, 60, 40, 0, Math.PI * 2, true);
-                    ctx.drawImage(pfp, x, 0, 100, 100);
+                    // const pfp = await Canvas.loadImage(
+                    //     msg.author.displayAvatarURL({extension: 'png'})
+                    // );
+                    // x = canvas.width / 5.8 - pfp.width;
+                    // y = canvas.height / 2 - pfp.height / 2;
+                    // ctx.arc(50, 60, 40, 0, Math.PI * 2, true);
+                    // ctx.drawImage(pfp, x, 0, 100, 100);
 
-                    ctx.fillStyle = '#FFFFFF';
-                    ctx.font = '27px Meiryo';
-                    let text = `${msg.author.username}#${msg.author.discriminator}`;
-                    x = canvas.width / 3 - pfp.width;
-                    y = canvas.height / 2.8 - pfp.height;
-                    ctx.fillText(text, x, y + pfp.height);
+                    // ctx.fillStyle = '#FFFFFF';
+                    // ctx.font = '27px Meiryo';
+                    // let text = `${msg.author.username}#${msg.author.discriminator}`;
+                    // x = canvas.width / 3 - pfp.width;
+                    // y = canvas.height / 2.8 - pfp.height;
+                    // ctx.fillText(text, x, y + pfp.height);
 
-                    ctx.font = '30px Meiryo';
-                    text = msg.content;
-                    if (msg.content.length > 35) ctx.font = '15px Meiryo';
-                    else if (msg.content.length > 50) ctx.font = '10px Meiryo';
-                    else if (msg.content.length > 100) ctx.font = '5px Meiryo';
-                    else ctx.font = '2px Meiryo';
-                    x = canvas.width / 4 - pfp.width / 2;
-                    y = canvas.height / 1.2 - pfp.height;
-                    ctx.fillText(text, x, y + pfp.height)
+                    // ctx.font = '30px Meiryo';
+                    // text = msg.content;
+                    // if (msg.content.length > 35) ctx.font = '15px Meiryo';
+                    // else if (msg.content.length > 50) ctx.font = '10px Meiryo';
+                    // else if (msg.content.length > 100) ctx.font = '5px Meiryo';
+                    // else ctx.font = '2px Meiryo';
+                    // x = canvas.width / 4 - pfp.width / 2;
+                    // y = canvas.height / 1.2 - pfp.height;
+                    // ctx.fillText(text, x, y + pfp.height)
 
-                    const attachment = new AttachmentBuilder(canvas.toBuffer());
+                    const MessageEmbed = new EmbedBuilder()
+                        .setColor('#59b9c6')
+                        .setAuthor({ name: msg.author.tag, iconURL: msg.author.displayAvatarURL({extension: 'png'}), url: msg.author.displayAvatarURL({extension: 'png'}) })
+                        .setDescription(msg.content)
+                        .setTimestamp(new Date(msg.createdAt))
+                        .setFooter({ text: '© 2021-2022 HitoriYuu, Hitrin' });
+
+                    // const attachment = new AttachmentBuilder(canvas.toBuffer());
+                    // await message.channel.send({
+                    //     files: [attachment]
+                    // });
+
                     await message.channel.send({
-                        files: [attachment]
+                        embeds: [MessageEmbed]
                     });
                 });
             }
